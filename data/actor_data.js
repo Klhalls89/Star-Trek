b4b852e440cbb070732c198449dfb9789d242d9a
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-26 15:19:12 -0600

    Add data files

---
+++ data/actor_data.js
@@ -0,0 +1,152 @@
+const actorData = [
+  {
+    name: 'Patrick Stewart',
+    show: ['TNG'],
+    nationality: 'British'
+  },
+  {
+    name: 'Jonathan Frakes',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Brent Spiner',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Gates McFadden',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Micheal Dorn',
+    show: ['TNG','DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'LeVar Burton',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Marina Sirtis',
+    show: ['TNG'],
+    nationality: 'British-American'
+  },
+  {
+    name: 'Wil Wheaton',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Whoopi Goldberg',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Denise Crosby',
+    show: ['TNG'],
+    nationality: 'American'
+  },
+  {
+    name: 'Avery Brooks',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Nana Visitor',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Terry Farrell',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Alexander Siddig',
+    show: ['DS9'],
+    nationality: 'British-Sudanese'
+  },
+  {
+    name: 'Colm Meany',
+    show: ['TNG','DS9'],
+    nationality: 'Irish'
+  },
+  {
+    name: 'Rene Auberjonois',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Armin Shimerman',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Andrew Robinson',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Cirroc Lofton',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Aron Eisenberg',
+    show: ['DS9'],
+    nationality: 'American'
+  },
+  {
+    name: 'Kate Mulgrew',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Robert Beltran',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Tim Russ',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Garrett Wang',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Robert Duncan McNeill',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Roxann Dawson',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Ethan Philips',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Jennifer Lien',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+  {
+    name: 'Jeri Ryan',
+    show: ['VOY'],
+    nationality: 'American-German'
+  },
+  {
+    name: 'Robert Picardo',
+    show: ['VOY'],
+    nationality: 'American'
+  },
+]
---
+++ data/trek_data.js
@@ -0,0 +1,152 @@
+const trekData = [
+  {
+    name: 'Jean-Luc Picard',
+    ship: ['Enterprise'],
+    rank: 'Capt.'
+  },
+  {
+    name: 'William Thomas Riker',
+    ship: ['Enterprise'],
+    rank: 'Cmdr.'
+  },
+  {
+    name: 'Data',
+    ship: ['Enterprise'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Beverly Crusher',
+    ship: ['Enterprise'],
+    rank: 'CMO'
+  },
+  {
+    name: 'Worf, son of Mogh',
+    ship: ['Enterprise', 'DS9'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Geordi La Forge',
+    ship: ['Enterprise'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Deanna Troi',
+    ship: ['Enterprise'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Wesley Crusher',
+    ship: ['Enterprise'],
+    rank: 'Ens.'
+  },
+  {
+    name: 'Guinan',
+    ship: ['Enterprise'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Natasha Yar',
+    ship: ['Enterprise'],
+    rank: 'Lt.'
+  },
+  {
+    name: 'Benjamin Sisko',
+    ship: ['DS9'],
+    rank: 'Capt.'
+  },
+  {
+    name: 'Kira Nerys',
+    ship: ['DS9'],
+    rank: 'Col.'
+  },
+  {
+    name: 'Jadzia Dax',
+    ship: ['DS9'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Julian Bashir',
+    ship: ['DS9'],
+    rank: 'CMO'
+  },
+  {
+    name: 'Miles O\'Brien',
+    ship: ['Enterprise', 'DS9'],
+    rank: 'Sr. Chf.'
+  },
+  {
+    name: 'Odo',
+    ship: ['DS9'],
+    rank: 'Cst.'
+  },
+  {
+    name: 'Quark',
+    ship: ['DS9'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Elim Garak',
+    ship: ['DS9'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Jake Sisko',
+    ship: ['DS9'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Nog',
+    ship: ['DS9'],
+    rank: 'Lt. Jr.'
+  },
+  {
+    name: 'Kathryn Janeway',
+    ship: ['Voyager'],
+    rank: 'Capt.'
+  },
+  {
+    name: 'Chakotay',
+    ship: ['Voyager'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Tuvok',
+    ship: ['Voyager'],
+    rank: 'Lt. Cmdr.'
+  },
+  {
+    name: 'Harry Kim',
+    ship: ['Voyager'],
+    rank: 'Ens.'
+  },
+  {
+    name: 'Thomas Eugene Paris',
+    ship: ['Voyager'],
+    rank: 'Lt. Jr.'
+  },
+  {
+    name: 'B\'Elanna Torres',
+    ship: ['Voyager'],
+    rank: 'Lt. Jr.'
+  },
+  {
+    name: 'Neelix',
+    ship: ['Voyager'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Kes',
+    ship: ['Voyager'],
+    rank: 'Civ.'
+  },
+  {
+    name: 'Seven of Nine',
+    ship: ['Voyager'],
+    rank: 'MCPO'
+  },
+  {
+    name: 'The Doctor',
+    ship: ['Voyager'],
+    rank: 'CMO'
+  }
+]
