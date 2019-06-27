f558037d5ec0c2441cafb57332bea1b7f1241258
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-27 13:57:42 -0600

    Finish migration

--- db/migrations/20190627125418_initial.js
+++ db/migrations/20190627125418_initial.js
@@ -1,16 +1,34 @@
 
 exports.up = function(knex) {
   return Promise.all([
-    knex.schema.createTable('startrek', function(table){
+    knex.schema.createTable('startrek', function(table) {
       table.increments('id').primary();
       table.string('name');
       table.string('actor');
       table.array('ship');
       table.string('rank');
+
+      table.timestamps(true, true)
+    }),
+
+    knex.schema.createTable('actors', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('character');
+      table.array('show');
+      table.string('nationality');
+      table.integer('startrek_id').unsigned()
+      table.foreign('startrek_id')
+        .references('startrek.data')
+
+      table.timestamps(true, true)
     })
   ])
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable('startrek'),
+    knex.schema.dropTable('actors')
+  ]);
 };
