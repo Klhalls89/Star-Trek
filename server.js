8fc79c56b9b322ada0eaa60ee669c1dd1b2cc944
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-28 11:06:47 -0600

    Get server running

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ app.listen(3000, () => {
  console.log(`App is running on 3000`)
 });
 
-app.get('/', function(request, response){
-  response.status(200).json('Engage!')
-});
+app.get('/', (request, response) => {
+  response.status(200).json('Engage')
+})
+
+app.get('/favicon.ico', (request, response) => {
+  response.status(200).json('Engage')
+})
 
